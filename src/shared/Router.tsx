e2939b78ac9164216a9c6b73ec36e5d9cb8f5dca
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-22 23:50:45 +0900

    Chore/Create main page

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,10 +1,11 @@
+import Main from 'pages/Main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
