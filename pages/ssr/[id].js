df23953d630c28aa1dc49cf605aeea80d3f62d92
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-10-28 23:03:25 +0900

    Feat/SSR

--- pages/ssr/[id].js
+++ pages/ssr/[id].js
@@ -15,3 +15,16 @@ const Post = ({ post }) => {
 };
 
 export default Post;
+
+export async function getServerSideProrps(context) {
+  const { params } = context;
+  const { id } = params;
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`
+  );
+  const post = await response.json();
+  console.log("Hello!");
+
+  return { props: { post } };
+}
