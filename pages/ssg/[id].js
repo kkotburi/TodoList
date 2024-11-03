b94bfb22192bd99457b947e1d194ebe7f1711e7d
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-11-03 23:39:38 +0900

    Feat/ISR

--- pages/ssg/[id].js
+++ pages/ssg/[id].js
@@ -29,6 +29,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`
+    // `https://localhost:4000/posts/${params.id}`
   );
   const post = await response.json();
 
