fd7c4584d5f2cf069da2351022b1af2d9f22ea0e
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-11-10 23:50:16 +0900

    Remove

--- pages/posts/[postId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-import React from "react";
-
-const Page = () => {
-  const router = useRouter();
-  return <div>Page: {router.query.postId}</div>;
-};
-
-export default Page;
