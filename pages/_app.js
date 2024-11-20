a0b231b6862f14ebe29dc19b8772d930f6d23e78
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-11-20 17:31:37 +0900

    Feat/Link

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <h1>Header</h1>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
