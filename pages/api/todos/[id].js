41b683f538dada38b5b1a35521bc058ff524c862
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-11-22 23:23:49 +0900

    Feat/api

---
+++ pages/api/todos/[id].js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const { query } = req;
+  const { id } = query;
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`
+  );
+
+  const todo = await response.json();
+
+  res.status(200).json({ data: todo });
+}
---
+++ pages/todos/[id].js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const todo = () => {
+  const [title, setTitle] = React.useState("");
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const { id } = router.query;
+    if (id) {
+      fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then((response) => response.json())
+        .then((json) => setTitle(json.title));
+    }
+  });
+
+  return <div>Todo: {title}</div>;
+};
+
+export default todo;
