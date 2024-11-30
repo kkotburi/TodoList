6728a1996e57469ae089032d7b49b1a15549f424
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-11-30 23:57:58 +0900

    Comment/_app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import "@/styles/globals.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   // console.log("pageProps", pageProps);
 
+  // useEffect(() => {
+  //   initialize;
+  // }, []);
+
   return (
     <>
+      {/* <Provider /> */}
       <h1>Header</h1>
       <div onClick={() => router.push("/")}>Home</div>
       <Link href="/about">About</Link>
